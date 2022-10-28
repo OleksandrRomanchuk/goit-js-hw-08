3875c9fcca7cd1e094fafd45e3d9501b86d5d6db
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-28 11:58:26 +0300

    task#2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import { saveDataToLocalSt, loadDataFromLocalSt } from './local-st-func';
+import throttle from 'lodash.throttle';
 
+//---------- refs ----------
+const iframeEL = document.querySelector('#vimeo-player');
+
+//---------- variables ----------
+const LOCAL_ST_KEY = 'videoplayer-current-time';
+const vimeoPlayer = new Player(iframeEL);
+
+//---------- code ----------
+Object.keys(localStorage).includes(LOCAL_ST_KEY)
+  ? vimeoPlayer.setCurrentTime(loadDataFromLocalSt(LOCAL_ST_KEY))
+  : false;
+
+vimeoPlayer.on('timeupdate', throttle(recordTimeToLocalSt, 1000));
+
+function recordTimeToLocalSt({ seconds }) {
+  saveDataToLocalSt(LOCAL_ST_KEY, seconds);
+}
