e949614a3880752de59875e83b36dcc8854308bc
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-28 09:55:29 +0300

    task#1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery > li > a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(data) {
+  return data
+    .map(({ preview, original, description }) => {
+      return `<li>
+                        <a class="gallery__item" href="${original}">
+                            <img class="gallery__image" src="${preview}" alt="${description}" />
+                        </a>
+                    </li>`;
+    })
+    .join('');
+}
