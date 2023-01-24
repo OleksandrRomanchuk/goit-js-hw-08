9b6533b839ca97f08511687faada91a6975feb42
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-24 11:48:58 +0200

    Update local-st-func.js

--- src/js/local-st-func.js
+++ src/js/local-st-func.js
@@ -1,27 +1,27 @@
-const saveDataToLocalSt = (key, value) => {
+function saveDataToLocalSt(key, value) {
   try {
     const serializedState = JSON.stringify(value);
     localStorage.setItem(key, serializedState);
   } catch (error) {
     console.error('Set state error: ', error.message);
   }
-};
+}
 
-const loadDataFromLocalSt = key => {
+function loadDataFromLocalSt(key) {
   try {
     const serializedState = localStorage.getItem(key);
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
     console.error('Get state error: ', error.message);
   }
-};
+}
 
-const removeDataFromLocalSt = key => {
+function removeDataFromLocalSt(key) {
   try {
     localStorage.removeItem(key);
   } catch (error) {
     console.error('Get state error: ', error.message);
   }
-};
+}
 
 export { saveDataToLocalSt, loadDataFromLocalSt, removeDataFromLocalSt };
